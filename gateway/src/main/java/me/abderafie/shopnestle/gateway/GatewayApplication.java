c201407c0a48eba407c1027a830ef26edcea5f46
Anasa <aziach.anas@student.emi.ac.ma>
2024-01-16 14:17:03 +0100

    update

--- gateway/src/main/java/me/abderafie/shopnestle/gateway/GatewayApplication.java
+++ gateway/src/main/java/me/abderafie/shopnestle/gateway/GatewayApplication.java
@@ -14,7 +14,7 @@ public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-
+	
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
