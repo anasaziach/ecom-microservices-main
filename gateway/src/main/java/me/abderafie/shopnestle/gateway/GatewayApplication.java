7d0f6c7e9d1a9240b2c88a972c7a2b3f702b5ac2
Anasa <aziach.anas@student.emi.ac.ma>
2024-01-16 22:31:52 +0100

    update

--- gateway/src/main/java/me/abderafie/shopnestle/gateway/GatewayApplication.java
+++ gateway/src/main/java/me/abderafie/shopnestle/gateway/GatewayApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 @SpringBootApplication
 public class GatewayApplication {
 
