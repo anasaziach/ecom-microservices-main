0dd9cfb422fa0684809207131beae2162fab6f1e
Anasa <aziach.anas@student.emi.ac.ma>
2024-01-17 11:31:37 +0100

    u

--- product-catalog-service/src/main/java/ma/ac/emi/ginfo/backend/config/WebConfig.java
+++ product-catalog-service/src/main/java/ma/ac/emi/ginfo/backend/config/WebConfig.java
@@ -6,11 +6,11 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-//@EnableWebMvc
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**");
-//    }
+   @Override
+   public void addCorsMappings(CorsRegistry registry) {
+       registry.addMapping("/**");
+   }
 }
