dd758f921a7ab74d7eecc6fc97433d4628b1bdd4
Anasa <aziach.anas@student.emi.ac.ma>
2024-01-18 11:45:16 +0100

    update category

--- product-catalog-service/src/main/java/ma/ac/emi/ginfo/backend/entity/Category.java
+++ product-catalog-service/src/main/java/ma/ac/emi/ginfo/backend/entity/Category.java
@@ -3,5 +3,12 @@ package ma.ac.emi.ginfo.backend.entity;
 public enum Category {
     MEN_CLOTHES,
     WOMEN_CLOTHES,
-    ELECTRONICS
+    ELECTRONICS,
+    PHONES,
+    AUDIO,
+    COMPUTERS,
+    PHOTOGRAPHY,
+    SMART_HOME,
+    TVS,
+    WEARABLES
 }
