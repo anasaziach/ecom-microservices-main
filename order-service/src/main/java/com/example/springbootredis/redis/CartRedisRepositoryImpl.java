c9f868708a6c39554593c7a93dfbe7b6ac9a341f
Anasa <aziach.anas@student.emi.ac.ma>
2024-01-18 02:02:17 +0100

    u

--- order-service/src/main/java/com/example/springbootredis/redis/CartRedisRepositoryImpl.java
+++ order-service/src/main/java/com/example/springbootredis/redis/CartRedisRepositoryImpl.java
@@ -6,13 +6,10 @@ import org.springframework.stereotype.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
-import redis.clients.jedis.Jedis;
 import java.util.ArrayList;
 import java.util.Collection;
 
 import com.example.springbootredis.entity.Item;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Repository
 public class CartRedisRepositoryImpl implements CartRedisRepository{
@@ -20,8 +17,6 @@ public class CartRedisRepositoryImpl implements CartRedisRepository{
 
 	private static final String ITEM = "ITEM";
 	private RedisTemplate<String, Object> redisTemplate;
-    private ObjectMapper objectMapper = new ObjectMapper();
-    private Jedis jedis = new Jedis();
 
 	private HashOperations<String,String, Collection<Item>> hashOperations;
 
@@ -60,17 +55,10 @@ public class CartRedisRepositoryImpl implements CartRedisRepository{
 
     @Override
     public void deleteItemFromCart(String key, Object item) {
-        try {
-            String itemCart = objectMapper.writeValueAsString(item);
-            jedis.srem(key, itemCart);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     public void deleteCart(String key) {
-        jedis.del(key);
     }
 
     @Override
